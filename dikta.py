814b06312aa18ae2785cbf974f64e54d7b6e6ea1
Styopa Semenukha <semenukha@gmail.com>
2017-01-03 21:44:49 -0500

    Added Chapter and changed JSON format

--- dikta.py
+++ dikta.py
@@ -1,5 +1,7 @@
+import json
+
 class Question:
-    def __init__(self, question, answers, multiple, keys):
+    def __init__(self, question, multiple, answers, keys):
         if type(question) is not str or not question:
             raise ValueError('Question must be a non-empty string')
 
@@ -19,3 +21,42 @@ class Question:
         self._answers = answers
         self._multiple = multiple
         self._keys = keys
+
+    def __str__(self):
+        return self._question
+
+class Chapter:
+    def __init__(self, title, questions):
+        self._title = title
+        self._questions = questions
+
+    def __str__(self):
+        return self._title
+
+class Quiz:
+    def __init__(self, json_file):
+        def object_factory(dct):
+            if 'question' in dct:
+                try:
+                    multiple = dct['multiple']
+                except(IndexError):
+                    multiple = False
+
+                return Question(
+                        dct['question'],
+                        multiple,
+                        dct['answers'],
+                        dct['keys'])
+
+        data = json.load(json_file, object_hook = object_factory)
+
+        self._title = data['title']
+        self._chapters = [];
+        for ch in data['quiz']:
+            self._chapters.append( Chapter(ch['chapter'], ch['questions']) )
+
+    def __len__(self):
+        return len(self._chapters)
+
+    def __str__(self):
+        return self._title
