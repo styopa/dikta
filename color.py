0270b6a2b76c6aa5ab100498a34d63d14893143c
Styopa Semenukha <semenukha@gmail.com>
2017-01-04 00:23:11 -0500

    Started writing color class

---
+++ color.py
@@ -0,0 +1,11 @@
+class Text:
+    def __init__(self, text, fg, bg)
+
+class ColorText:
+    (black, red, green, yellow, blue, magenta, cyan, white) = range(8)
+
+    def __init__(self, text):
+        self._text = text
+
+    def __str__(self):
+
