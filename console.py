37f94a633d746f7f118e80abda82f89350c4d2e4
Styopa Semenukha <semenukha@gmail.com>
2017-01-06 00:51:54 -0500

    Started using color lib for output

--- console.py
+++ console.py
@@ -1,36 +1,15 @@
 import dikta
+import color
 
 class Console:
     def __init__(self, quiz):
         self._quiz = quiz
 
     def run_quiz(self):
-        def color_print(msg, fg = None, bg = None, bold = False):
-            if fg is None:
-                fg_esc = ''
-            else:
-                if bold:
-                    bold_esc = 1
-                else:
-                    bold_esc = 0
-
-                fg_esc = "\033[%i;%im" % (bold_esc, 30 + fg)
-
-            if bg is None:
-                bg_esc = ''
-            else:
-                bg_esc = "\033[" + str(40 + bg) + "m"
-
-            reset = "\033[0m"
-
-            print(bg_esc + fg_esc + msg + reset)
-
-        (BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE) = range(8)
-
-        color_print(str(self._quiz), fg = MAGENTA, bold = True)
+        print( color.Text(self._quiz).bg(color.magenta).fg(color.white).bold(True) )
 
         for chapter in self._quiz:
-            color_print(str(chapter), fg = BLUE, bold = True)
+            print( color.Text(chapter).fg(color.blue).bold(True) )
 
             for question in chapter:
                 print(question)
@@ -39,9 +18,9 @@ class Console:
                     print("  %s. %s" % (idx.upper(), question.options[idx]))
 
                 if question.multiple:
-                    prompt = 'Choose multiple options: '
+                    opt = 'multiple'
                 else:
-                    prompt = 'Choose one option: '
-                res = input(prompt)
+                    opt = 'one'
+                res = input("Choose %s: " % opt)
 
                 print()
