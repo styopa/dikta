be3ebd56fe68a71e79438704eaad52380634d671
Styopa Semenukha <semenukha@gmail.com>
2017-01-08 13:55:28 -0500

    Moved asking question to a function

--- console.py
+++ console.py
@@ -6,21 +6,33 @@ class Console:
         self._quiz = quiz
 
     def run_quiz(self):
+        def print_question(question):
+            print(question)
+
+            for idx in sorted(question.options.keys()):
+                print("  %s. %s" % (idx.upper(), question.options[idx]))
+
+        def filter_answer(question):
+            if question.multiple:
+                opt = 'multiple'
+            else:
+                opt = 'one'
+            while True:
+                try:
+                    response = input("Choose %s: " % opt)
+                    question.answer(response)
+                    break
+                except(dikta.
+
         print( color.Text(self._quiz).bg(color.magenta).fg(color.white).bold(True) )
 
         for chapter in self._quiz:
             print( color.Text(chapter).fg(color.blue).bold(True) )
 
             for question in chapter:
-                print(question)
-
-                for idx in sorted(question.options.keys()):
-                    print("  %s. %s" % (idx.upper(), question.options[idx]))
-
-                if question.multiple:
-                    opt = 'multiple'
-                else:
-                    opt = 'one'
-                res = input("Choose %s: " % opt)
+                print_question(question)
 
                 print()
+
+    def show_results(self):
+        pass
